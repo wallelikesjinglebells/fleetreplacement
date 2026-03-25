fcb646e1e31434986db1d27ef5c03b8308e49ecf
Jing Wang <jing.wang@tum.de>
2026-03-25 11:40:35 +0100

    Changed config variables to load from config.py

--- fleetreplacement_env/envs/fleet_replacement.py
+++ fleetreplacement_env/envs/fleet_replacement.py
@@ -3,50 +3,31 @@ import gymnasium as gym
 from gymnasium import spaces
 # import pygame
 import numpy as np
-from dataclasses import dataclass
-
-# Dataclass for flexible parameter adjustment
-@dataclass
-class FleetConfig:
-    n_vehicles: int = 10            # fleet size
-    max_vehicle_age: int = 20       # max truck age in years before forced retirement
-    max_mileage: int = 500_000      # max truck mileage in km before forced retirement
-    planning_horizon: int = 10      # planning time horizon in years (when is one training episode over?)
-    annual_mileage: int = 50_000    # one vehicle's annual mileage in km
-
-    # Simple cost parameters
-    purchase_cost_bet: float = 150_000      # purchase cost for BET in €
-    purchase_cost_dt: float = 100_000       # purchase cost for DT in €
-    base_maintenance_cost: float = 5_000    # maintenance cost per year in €
-    maintenance_age_factor: float = 500     # extra cost per year of age in €
-    fuel_cost_per_km: float = 0.35          # cost for diesel per km in €
-    electricity_cost_per_km: float = 0.08   # cost for electricity per km in €
-    salvage_value_base: float = 50_000      # new truck salvage value in €
-    salvage_depreciation: float = 0.85      # factor per year of truck age
+# from dataclasses import dataclass
+from fleetreplacement_env.envs.config import FleetEnvConfig, MDPConfig, load_cost_config
 
 class FleetReplacementEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 1}  # for rendering
 
-    def __init__(self, config: FleetConfig | None = None, render_mode: str | None = None):
+    def __init__(self, config: FleetEnvConfig | None = None, render_mode: str | None = None):
         super().__init__()
 
-        self.config = config or FleetConfig()
+        # If no config is pased, build a default config by loading from CSV files
+        if config is None:
+            config = FleetEnvConfig(
+                mdp = MDPConfig(),
+                cost = load_cost_config()     # call loading config
+            )
+        self.cfg = config                     # holds self.cfg.mdp and self.cfg.cost
+
         self.current_step = 0
         self.fleet_state: np.ndarray | None = None  # represent unitialized state, for guard in step()
         self.render_mode = render_mode              # for rendering
 
         # Unpack config calues
-        n_vehicles = self.config.n_vehicles
-        max_vehicle_age = self.config.max_vehicle_age
-        max_mileage = self.config.max_mileage
-        # annual_mileage = self.config.annual_mileage
-        # salvage_value_base = self.config.salvage_value_base
-        # salvage_depreciation = self.config.salvage_depreciation
-        # purchase_cost = self.config.purchase_cost
-        # fuel_cost_per_km = self.config.fuel_cost_per_km
-        # base_maintenance_cost = self.config.base_maintenance_cost
-        # maintenance_age_factor = self.config.maintenance_age_factor
-        # planning_horizon = self.config.planning_horizon
+        n_vehicles = self.cfg.mdp.n_vehicles
+        max_vehicle_age = self.cfg.mdp.max_vehicle_age
+        max_mileage = self.cfg.mdp.max_mileage
         
         # State space as box, matrix of shape (n_vehicles, 3 (technology, age, mileage))
         # After including current_step in state space: vector with n_vehicles*3+1 elements
@@ -63,11 +44,12 @@ class FleetReplacementEnv(gym.Env):
     # Constructing observations for NN
     def _get_obs(self):
         tech = self.fleet_state[:, 0]
-        age = self.fleet_state[:, 1] / self.config.max_vehicle_age                                          # normalize
-        mileage = self.fleet_state[:, 2] / self.config.max_mileage                                          # normalize
+        age = self.fleet_state[:, 1] / self.cfg.mdp.max_vehicle_age                                          # normalize
+        mileage = self.fleet_state[:, 2] / self.cfg.mdp.max_mileage                                          # normalize
         flat_fleet = np.stack([tech, age, mileage], axis=1).flatten().astype(np.float32)        
-        step_feature = np.array([self.current_step / self.config.planning_horizon], dtype=np.float32)       # normalize
+        step_feature = np.array([self.current_step / self.cfg.mdp.planning_horizon], dtype=np.float32)       # normalize
         return np.concatenate([flat_fleet, step_feature])
+    
     def _get_info(self):
         return {
             "step": self.current_step,
@@ -82,9 +64,9 @@ class FleetReplacementEnv(gym.Env):
         super().reset(seed=seed)
         self.current_step = 0
 
-        ages = self.np_random.integers(0, 10, size=self.config.n_vehicles).astype(np.float32)      # generate random vehicle age, convert to float (as defined in obs space)
-        mileages = ages * self.config.annual_mileage                                               # starting mileage, derived from age                                            
-        technologies = np.zeros(self.config.n_vehicles, dtype=np.float32)                          # 0 = diesel, all DT
+        ages = self.np_random.integers(0, 10, size=self.cfg.mdp.n_vehicles).astype(np.float32)      # generate random vehicle age, convert to float (as defined in obs space)
+        mileages = ages * self.cfg.cost.akt_base                                         # starting mileage, derived from age                                            
+        technologies = np.zeros(self.cfg.mdp.n_vehicles, dtype=np.float32)                          # 0 = diesel, all DT
 
         self.fleet_state = np.stack([technologies, ages, mileages], axis=1)            # combine above arrays to matrix of shape (n_vehicles, 3) to make columns parameters, rows vehicles
 
@@ -99,9 +81,9 @@ class FleetReplacementEnv(gym.Env):
         action = np.asarray(action, dtype=np.int32)
         total_cost = 0.0    # initialize cost
 
-        resolved_action = np.empty(self.config.n_vehicles, dtype=np.int32)      # initializes array to store actual action (override or agent)
+        resolved_action = np.empty(self.cfg.mdp.n_vehicles, dtype=np.int32)      # initializes array to store actual action (override or agent)
 
-        for i in range(self.config.n_vehicles):
+        for i in range(self.cfg.mdp.n_vehicles):
             tech, age, mileage = self.fleet_state[i]    # unpack row i (vehicle i) into three variables
             # replace = bool(action[i])                   # convert binary action into true/false
             act = int(action[i])                        # assign value of replacement to act (0 = keep, 1 = replace with DT, 2 = replace with BET)
@@ -109,8 +91,8 @@ class FleetReplacementEnv(gym.Env):
             # Forced replacement if limits exceeded (regardless of action)
             # Returns true or false if one is true
             force_replace = (
-                age + 1 >= self.config.max_vehicle_age
-                or mileage + self.config.annual_mileage >= self.config.max_mileage
+                age + 1 >= self.cfg.mdp.max_vehicle_age
+                or mileage + self.cfg.cost.akt_base >= self.cfg.mdp.max_mileage
             )
 
             # If agent says no replacement, but force_replace is true, default to replace with DT
@@ -121,29 +103,29 @@ class FleetReplacementEnv(gym.Env):
 
             # Replace with DT
             if act == 1:
-                salvage = self.config.salvage_value_base * (self.config.salvage_depreciation ** age)
-                total_cost += self.config.purchase_cost_dt - salvage
+                salvage = self.cfg.mdp.salvage_value_base * (self.cfg.mdp.salvage_depreciation ** age)
+                total_cost += self.cfg.mdp.purchase_cost_dt - salvage
                 self.fleet_state[i] = [0.0, 0.0, 0.0]
 
             # Replace with BET
             elif act == 2:
-                salvage = self.config.salvage_value_base * (self.config.salvage_depreciation ** age)
-                total_cost += self.config.purchase_cost_bet - salvage
+                salvage = self.cfg.mdp.salvage_value_base * (self.cfg.mdp.salvage_depreciation ** age)
+                total_cost += self.cfg.mdp.purchase_cost_bet - salvage
                 self.fleet_state[i] = [1.0, 0.0, 0.0]
 
             # Keep
             else:
-                maintenance = self.config.base_maintenance_cost + self.config.maintenance_age_factor * age
+                maintenance = self.cfg.mdp.base_maintenance_cost + self.cfg.mdp.maintenance_age_factor * age
                 if tech == 0.0:     # keep DT
-                    running_cost = self.config.annual_mileage * self.config.fuel_cost_per_km
+                    running_cost = self.cfg.cost.akt_base * self.cfg.mdp.fuel_cost_per_km
                 else:               # keep BET
-                    running_cost = self.config.annual_mileage * self.config.electricity_cost_per_km
+                    running_cost = self.cfg.cost.akt_base * self.cfg.mdp.electricity_cost_per_km
                 total_cost += running_cost + maintenance
-                self.fleet_state[i] = [tech, age + 1, mileage + self.config.annual_mileage]
+                self.fleet_state[i] = [tech, age + 1, mileage + self.cfg.cost.akt_base]
 
         self.current_step += 1
         reward = -total_cost    # agent's reward is negative cost
-        truncated = self.current_step >= self.config.planning_horizon
+        truncated = self.current_step >= self.cfg.mdp.planning_horizon
         terminated = False  
 
         # Rendering with declared mode
