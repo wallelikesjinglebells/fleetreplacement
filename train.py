2005fa6ff5016a9cc968dc1e9878f0c17d362776
Jing Wang <jing.wang@tum.de>
2026-03-28 14:00:18 +0100

    Normalized reward

--- train.py
+++ train.py
@@ -5,10 +5,13 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import ProgressBarCallback
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
+from stable_baselines3.common.vec_env import sync_envs_normalization
+
 
 # Configuration variables, tunable settings
 ENV_ID        = "FleetReplacement-v0"
-TOTAL_STEPS   = 1_500_000   # total number of environment steps to train (increase for real training)
+TOTAL_STEPS   = 5_000_000   # total number of environment steps to train (increase for real training)
 N_ENVS        = 4         # parallel environments for faster data collection
 EVAL_FREQ     = 10_000    # pause training every EVAL_FREQ steps to evaluate current policy on eval_env
 LOG_DIR       = "./logs/"
@@ -16,9 +19,15 @@ SAVE_PATH     = "./models/ppo_fleet"
 
 # Create vectorized environment for all N_ENVS environments
 vec_env = make_vec_env(ENV_ID, n_envs=N_ENVS)
+vec_env = VecNormalize(vec_env, norm_obs=False, norm_reward=True, gamma=0.99)       # normalize reward
 
 # Single evaluation environment
-eval_env = Monitor(gym.make(ENV_ID), filename="./logs/eval_monitor")        # Monitor wrapper to fix warning if other wrappers are present
+_monitor_env = Monitor(gym.make(ENV_ID), filename="./logs/eval_monitor")        # Monitor wrapper to fix warning if other wrappers are present
+eval_env = DummyVecEnv([lambda: _monitor_env])
+eval_env = VecNormalize(eval_env, norm_obs=False, norm_reward=True,
+                        training=False, gamma=0.99)
+# Sync stats from training env
+sync_envs_normalization(vec_env, eval_env)
 
 # Callback: every EVAL_FREQ, run current policy on eval_env, record mean reward, if better than previous best 
 eval_callback = EvalCallback(
@@ -51,19 +60,3 @@ model = PPO.load(f"{SAVE_PATH}/best_model")                                     
 # Save final model at end of training
 model.save(f"{SAVE_PATH}_final")
 print(f"Training complete. Model saved to {SAVE_PATH}_final")
-
-# Sanity check :) run one full episode with trained policy
-env = gym.make(ENV_ID, render_mode="human")
-# obs, info = env.reset(seed=0)                                 # uncomment if fleet should stay the same
-obs, info = env.reset()                                         # random fleet composition
-done = False
-total_reward = 0.0
-
-while not done:
-    action, _ = model.predict(obs, deterministic=True)              # pick greedy action
-    obs, reward, terminated, truncated, info = env.step(action)
-    total_reward += reward
-    done = terminated or truncated
-
-print(f"Evaluation episode total reward: €{total_reward:,.0f}")
-env.close()
